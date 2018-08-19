fc2a3736b951a39c4af655ed3a26413a385de20e
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2018-08-19 01:21:44 -0400

    enable custom colors with color-picker

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,6 @@
 const addMapping = document.getElementById('add-mapping'),
-			goToOptions = document.getElementById('go-to-options'),
-			colors = document.getElementById('color-select');
+		goToOptions = document.getElementById('go-to-options'),
+		colors = document.getElementById('color-select');
 
 var colorMappings,
     hostName;
@@ -26,88 +26,23 @@ function getHostName(tabsObject) {
 //save mapping
 addMapping.addEventListener( 'click', function() {
 	colorMappings[hostName] = colors.value;
-	browser.storage.local.set({colorMappings});
-	browser.theme.update( themes[colors.value] );
+	browser.storage.local.set( {colorMappings} );
+	browser.theme.update(
+		{ colors: {
+		     accentcolor: colors.value,
+		     textcolor: '#000',
+		    }
+		}
+	);
 });
 
 //set dropdown to value appropriate value if hostname already saved
 Promise.all( [loadMappingsPromise, getHostNamePromise] ).then( function() {
 		if ( colorMappings[hostName] ) {
 			colors.value = colorMappings[hostName];
-		} 
+		}
 }	);
 
 function onError(error) {
   console.log(`Error: ${error}`);
 }
-
-const themes = {
-  'grey': {
-    colors: {
-     accentcolor: '#000',
-     textcolor: '#FFF',
-    }
-  },
-  'white': {
-    colors: {
-     accentcolor: '#FFF',
-     textcolor: '#000',
-    }
-  },
-  'red': {
-    colors: {
-     accentcolor: '#ff0000',
-     textcolor: '#000',
-    }
-  },
-  'orange': {
-    colors: {
-     accentcolor: '#ffa500',
-     textcolor: '#000',
-    }
-  },
-  'yellow': {
-    colors: {
-     accentcolor: '#ffff00',
-     textcolor: '#000',
-    }
-  },
-  'green': {
-    colors: {
-     accentcolor: '#00ff00',
-     textcolor: '#000',
-    }
-  },
-  'dark-green': {
-    colors: {
-     accentcolor: '#006400',
-     textcolor: '#000',
-    }
-  },
-  'blue': {
-    colors: {
-     accentcolor: '#0E4D92',
-     textcolor: '#000',
-    }
-  },
-  'teal': {
-    colors: {
-     accentcolor: '#008080',
-     textcolor: '#000',
-    }
-  },
-  'purple': {
-    colors: {
-     accentcolor: '#551a8b',
-     textcolor: '#000',
-    }
-  }
-};
-
-
-
-
-
-
-
-
