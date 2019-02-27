2de7d6df89e31344be49db8b38dad1d5ab19779a
mikko001 <48047613+mikko001@users.noreply.github.com>
2019-02-27 12:46:34 +0200

    Update popup.js
    Add color mapping for an app (app == first directory in url after domain)

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,13 @@
 const addMapping = document.getElementById('add-mapping'),
 	  goToOptions = document.getElementById('go-to-options'),
-	  colors = document.getElementById('color-select');
+	  colors = document.getElementById('color-select'),
+	  addMappingApp = document.getElementById('add-mapping-app'),
+	  colorsApp = document.getElementById('color-select-app')
+	  ;
 
 var colorMappings,
-    host;
+    host,
+    app;
 
 goToOptions.addEventListener( 'click', function() {
 	browser.runtime.openOptionsPage();
@@ -17,6 +21,8 @@ function getHostName() {
 function setHostName( tabsObject ) {
 	var currentURL = new URL( tabsObject[0].url );
 	host = currentURL.host;
+	var arrPath = currentURL.pathname.split("/");
+	app = arrPath[1];
 	//match color of input to saved domain
 	browser.storage.local.get( 'colorMappings' ).then( function( item ) {
 		colorMappings = item.colorMappings || {};
@@ -25,6 +31,11 @@ function setHostName( tabsObject ) {
 		} else {
 			colors.value = '#FFFFFF';
 		}
+		if ( colorMappings[ host +"/"+ app ] ) {
+			colorsApp.value = colorMappings[ host +"/"+ app ];
+		} else {
+			colorsApp.value = '#FFFFFF';
+		}		
 	});
 }
 
@@ -56,6 +67,18 @@ addMapping.addEventListener( 'click', function() {
 	browser.theme.update(
 		{ colors: {
 		     frame: colors.value,
+		     backgroundtext: '#000',
+		    }
+		}
+	);
+});
+
+addMappingApp.addEventListener( 'click', function() {
+	colorMappings[ host +"/"+ app ] = colorsApp.value;
+	browser.storage.local.set( {colorMappings} );
+	browser.theme.update(
+		{ colors: {
+		     frame: colorsApp.value,
 		     backgroundtext: '#000',
 		    }
 		}
