def7c99b8cd3e8885feb5a66210323cca64d8ebd
Andreas Horn <mail@andreashorn.net>
2024-02-17 01:28:50 +0100

    implements first draft for using regular expressions for applying a color mapping

--- scripts/switcher.js
+++ scripts/switcher.js
@@ -1,57 +1,71 @@
 var host,
-    hostname,
-    port;
+  hostname,
+  port,
+  href;
 
-browser.tabs.onUpdated.addListener( handleUpdated) ;
-browser.tabs.onActivated.addListener( handleActivated );
-browser.windows.onFocusChanged.addListener( handleActivated );
+browser.tabs.onUpdated.addListener(handleUpdated);
+browser.tabs.onActivated.addListener(handleActivated);
+browser.windows.onFocusChanged.addListener(handleActivated);
 
 function handleUpdated(tabId, changeInfo, tab) {
-  if( changeInfo.status === 'complete') {
-  	browser.tabs.query({currentWindow: true, active: true}).then(getURL, onError);
+  if (changeInfo.status === 'complete') {
+    browser.tabs.query({ currentWindow: true, active: true }).then(getURL, onError);
   }
 }
 
-function handleActivated( e ){
-	browser.tabs.query({currentWindow: true, active: true}).then(getURL, onError);
+function handleActivated(e) {
+  browser.tabs.query({ currentWindow: true, active: true }).then(getURL, onError);
 }
 
-function getURL( tabs ) {
-    var currentURL = new URL( tabs[0].url );
+function getURL(tabs) {
+  var currentURL = new URL(tabs[0].url);
 
-    host = currentURL.host;
-    hostname = currentURL.hostname;
-    port = currentURL.port;
+  host = currentURL.host;
+  hostname = currentURL.hostname;
+  port = currentURL.port;
+  href = currentURL.href;
 
-    switchColor();
+  switchColor();
 }
 
-function changeTheme( entry, mappings ) {
-  browser.theme.update( { colors: {
-     frame: mappings[ entry ],
-     backgroundtext: '#000',
+function changeTheme(entry, mappings) {
+  browser.theme.update({
+    colors: {
+      frame: mappings[entry],
+      backgroundtext: '#000',
     }
   });
 }
 
 function switchColor() {
-	var colorMappings = browser.storage.local.get( 'colorMappings' );
-	colorMappings.then( function( item ) {
-		colorMappings = item.colorMappings || {};
-		if ( colorMappings[ host ] ) {
-		  changeTheme( host, colorMappings );
-		} else if ( port.length !== 0 && colorMappings[ hostname ] ) {
-      changeTheme( hostname, colorMappings );
-    } else {
+  var colorMappings = browser.storage.local.get('colorMappings');
+  colorMappings.then(function (item) {
+    colorMappings = item.colorMappings || {};
+
+    let foundMatch = false;
+
+    for (let hostRegexpString of Object.keys(colorMappings)) {
+      let hostRegexp = new RegExp(hostRegexpString);
+
+      console.log(href);
+
+      if (href.match(hostRegexp)) {
+        changeTheme(hostRegexpString, colorMappings);
+        foundMatch = true;
+        break;
+      }
+    }
+
+    if (!foundMatch) {
       browser.theme.reset();
     }
-	}, onError);
+  }, onError);
 }
 
 //open sidebar from Toolbar Button (aka browser action)
-browser.browserAction.onClicked.addListener( function() {
-	browser.sidebarAction.open();
-} );
+browser.browserAction.onClicked.addListener(function () {
+  browser.sidebarAction.open();
+});
 
 function onError(error) {
   console.log(`Error: ${error}`);
